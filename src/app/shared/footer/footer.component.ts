d893fc6b0e5e96da99afeadbd8ca46cec88f5b91
NallelyLG <Z406162@isban.mx>
2019-03-05 17:56:23 -0600

    directivas

---
+++ src/app/shared/footer/footer.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+  
+  public botones: any[]=[{
+    name: 'Boton 1'
+  },{
+    name: 'Boton 2'
+  },{
+    name: 'Boton 3'
+  }];
+
+  public promotion: boolean= true;
+  public color: boolean= false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public aplicarPromocion(){
+    this.color= !this.color;
+    
+  }
+
+}
